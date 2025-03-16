8632cff1b3c492c119e2e961e2abd2115cb67cf6
朝倉信 <makotoasakura165191@gmail.com>
2025-03-16 13:20:28 +0900

    ユーザーが記事を所有するよう設定

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2025_03_08_043852) do
-ActiveRecord::Schema.define(version: 2025_03_12_134921) do
 
   create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
+    t.integer "user_id", null: false
+    t.string "title", null: false
+    t.text "content", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-10.times do
-  Article.create(
+haruka = User.create!(email: 'haruka@poke.com', password: 'achamo')
+matoro = User.create!(email: 'matoro@inazuma.com', password: 'hakuren')
+
+5.times do
+  haruka.articles.create!(
+    title: Faker::Lorem.sentence(word_count: 5),
+    content: Faker::Lorem.sentence(word_count: 100)
+  )
+end
+
+5.times do
+  matoro.articles.create!(
     title: Faker::Lorem.sentence(word_count: 5),
     content: Faker::Lorem.sentence(word_count: 100)
   )
